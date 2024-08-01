492eca5a1ed2dc6ca5678987c0dbdf10101f47f5
gis4pl <m.zygadlo@gis4.pl>
2024-08-01 17:34:19 +0200

    Add v 0.2 files

---
+++ i18n/Sections.pro
@@ -0,0 +1,8 @@
+FORMS = ../sections_dialog_base.ui
+SOURCES = ../sections.py \
+          ../sections_dialog.py
+TRANSLATIONS = Sections_pl.ts \
+               Sections_it.ts \
+               Sections_fr.ts \
+               Sections_de.ts \
+               Sections_es.ts
---
+++ i18n/Sections_it.ts
@@ -0,0 +1,120 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it" sourcelanguage="en">
+<context>
+    <name>Sections</name>
+    <message>
+        <location filename="../sections.py" line="188"/>
+        <source>&amp;Sections</source>
+        <translation>&amp;Sezioni</translation>
+    </message>
+    <message>
+        <location filename="../sections.py" line="175"/>
+        <source>Sections from points</source>
+        <translation>Sezioni da punti</translation>
+    </message>
+</context>
+<context>
+    <name>SectionsDialogBase</name>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="14"/>
+        <source>Sections</source>
+        <translation>Sezioni</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="42"/>
+        <source>Define margins between print format and map</source>
+        <translation>Definisci i margini tra il formato di stampa e la mappa</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="45"/>
+        <source>Margins</source>
+        <translation>Margini</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="73"/>
+        <source>Define the bottom margin</source>
+        <translation>Definisci il margine inferiore</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="86"/>
+        <source>Define the left margin</source>
+        <translation>Definisci il margine sinistro</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="99"/>
+        <source>Define the right margin</source>
+        <translation>Definisci il margine destro</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="112"/>
+        <source>Define the top margin</source>
+        <translation>Definisci il margine superiore</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="125"/>
+        <source>Bottom</source>
+        <translation>Buco</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="141"/>
+        <source>Right</source>
+        <translation>Destro</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="157"/>
+        <source>Top</source>
+        <translation>Superiore</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="173"/>
+        <source>Left</source>
+        <translation>Sinistra</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="190"/>
+        <source>1 : </source>
+        <translation>1 : </translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="203"/>
+        <source>Select a layer</source>
+        <translation>Seleziona un livello</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="216"/>
+        <source>Select the layer for which you want to get sections</source>
+        <translation>Seleziona il livello per il quale vuoi ottenere le sezioni</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="229"/>
+        <source>Print format</source>
+        <translation>Formato di stampa</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="242"/>
+        <source>Print orientation</source>
+        <translation>Orientamento di stampa</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="255"/>
+        <source>Select the format in which you will print the sections</source>
+        <translation>Seleziona il formato in cui vuoi stampare le sezioni</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="268"/>
+        <source>Select the orientation of the printed sections</source>
+        <translation>Selezionare l&apos;orientamento delle sezioni stampate</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="281"/>
+        <source>Set map scale</source>
+        <translation>Imposta la scala della mappa</translation>
+    </message>
+    <message>
+        <location filename="../sections_dialog_base.ui" line="294"/>
+        <source>Select the scale of the maps in the sections</source>
+        <translation>Seleziona la scala delle mappe nelle sezioni</translation>
+    </message>
+</context>
+</TS>
